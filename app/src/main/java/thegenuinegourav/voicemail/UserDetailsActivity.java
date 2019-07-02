4199d6027bbe404a1c1085e0d3c40ab87b5cdf40
G Uma Maheswari <gumaheswari16@gmail.com>
2019-07-02 15:50:07 +0530

    user store data

--- app/src/main/java/thegenuinegourav/voicemail/UserDetailsActivity.java
+++ app/src/main/java/thegenuinegourav/voicemail/UserDetailsActivity.java
@@ -1,13 +1,194 @@
 package thegenuinegourav.voicemail;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Handler;
+import android.speech.RecognizerIntent;
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+import java.util.ArrayList;
+import java.util.Locale;
 
 public class UserDetailsActivity extends AppCompatActivity {
+
+    private TextToSpeech tts;
+    private TextView status;
+    private TextView From,Password;
+    private int numberOfClicks;
+    UserLocalStore userLocalStore;
+    private boolean IsInitialVoiceFinshed;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user_details);
+        setContentView(R.layout.activity_main);
+        IsInitialVoiceFinshed = false ;
+        tts = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status == TextToSpeech.SUCCESS) {
+                    int result = tts.setLanguage(Locale.US);
+                    if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                        Log.e("TTS", "This Language is not supported");
+                    }
+                    speak("Welcome to voice mail");
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                        }
+                    }, 1000);
+                } else {
+                    Log.e("TTS", "Initilization Failed!");
+                }
+            }
+        });
+
+        status = (TextView)findViewById(R.id.status);
+        From = (TextView)findViewById(R.id.from);
+        Password =(TextView)findViewById((R.id.password));
+
+        userLocalStore = new UserLocalStore(this);
+        numberOfClicks = 0;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (userLocalStore.getLoggedInUser() == null){
+            speak("Tell me your mail address? or cancel to close the application ");
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    status.setText("Mail?");
+                    IsInitialVoiceFinshed = true;
+                }
+            }, 4000);
+        }
+        else {
+
+
+        }
+    }
+    private void speak(String text){
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            tts.speak(text, TextToSpeech.QUEUE_FLUSH, null, null);
+        }else{
+            tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        if (tts != null) {
+
+            tts.stop();
+            tts.shutdown();
+        }
+        super.onDestroy();
+    }
+
+    public void layoutClicked(View view)
+    {
+        if(IsInitialVoiceFinshed) {
+            numberOfClicks++;
+            listen();
+        }
+    }
+
+    private void listen(){
+        Intent i = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        i.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        i.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        i.putExtra(RecognizerIntent.EXTRA_PROMPT, "Say something");
+
+        try {
+            startActivityForResult(i, 100);
+        } catch (ActivityNotFoundException a) {
+            Toast.makeText(UserDetailsActivity.this, "Your device doesn't support Speech Recognition", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    private void exitFromApp()
+    {
+        this.finishAffinity();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 100 && IsInitialVoiceFinshed) {
+            IsInitialVoiceFinshed = false;
+            if (resultCode == RESULT_OK && null != data) {
+                ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                if (result.get(0).equals("cancel")) {
+                    speak("Cancelled!");
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            exitFromApp();
+                        }
+                    }, 4000);
+
+                } else {
+
+                    switch (numberOfClicks) {
+                        case 1:
+                            String from;
+                            from = result.get(0).replaceAll("underscore", "_");
+                            from = from.replaceAll("\\s+", "");
+                            From.setText(from);
+                            Config.EMAIL = from;
+                            status.setText("Password?");
+                            speak("Password?");
+                            break;
+                        case 2:
+                            String pwd;
+                            pwd = result.get(0).replaceAll("\\s+","");
+                            Config.PASSWORD = pwd;
+                            Password.setText(pwd);
+                            status.setText("Confirm?");
+                            speak("Please Confirm the mail\n User : " + From.getText().toString() + "your password" +Config.PASSWORD + "\nSpeak Yes to confirm");
+                            break;
+                        default:
+                             if(result.get(0).equals("yes")||result.get(0).equals("s"))
+                             {
+                                 User user;
+                                 String username = From.getText().toString();
+                                 String password = Password.getText().toString();
+
+                                 user = new User(username, password);
+                                 userLocalStore.storeUserData(user);
+                                 userLocalStore.setUserLoggedIn(true);
+                                 Intent HomeIntent = new Intent(UserDetailsActivity.this,MainActivity.class);
+                                 startActivity(HomeIntent);
+                                 finish();
+                             }else
+                             {
+                                 status.setText("Mail?");
+                                 speak("Please provide your Mail?");
+                                 new Handler().postDelayed(new Runnable() {
+                                     @Override
+                                     public void run() {
+                                         numberOfClicks = 0;
+                                     }
+                                 }, 2000);
+                             }
+                             break;
+
+
+                    }
+                }
+            }
+            IsInitialVoiceFinshed = true;
+        }
     }
 }
---
+++ app/src/main/java/thegenuinegourav/voicemail/UserLocalStore.java
@@ -0,0 +1,46 @@
+package thegenuinegourav.voicemail;
+
+import  android.content.Context;
+import  android.content.SharedPreferences;
+
+public class UserLocalStore {
+
+    public static final String SP_NAME = "userDetails";
+
+    SharedPreferences userLocalDatabase;
+
+    public UserLocalStore(Context context) {
+        userLocalDatabase = context.getSharedPreferences(SP_NAME, 0);
+    }
+
+    public void storeUserData(User user) {
+        SharedPreferences.Editor userLocalDatabaseEditor = userLocalDatabase.edit();
+        userLocalDatabaseEditor.putString("username", user.username);
+        userLocalDatabaseEditor.putString("password", user.password);
+        userLocalDatabaseEditor.commit();
+    }
+
+    public void setUserLoggedIn(boolean loggedIn) {
+        SharedPreferences.Editor userLocalDatabaseEditor = userLocalDatabase.edit();
+        userLocalDatabaseEditor.putBoolean("loggedIn", loggedIn);
+        userLocalDatabaseEditor.commit();
+    }
+
+    public void clearUserData() {
+        SharedPreferences.Editor userLocalDatabaseEditor = userLocalDatabase.edit();
+        userLocalDatabaseEditor.clear();
+        userLocalDatabaseEditor.commit();
+    }
+
+    public User getLoggedInUser() {
+        if (userLocalDatabase.getBoolean("loggedIn", false) == false) {
+            return null;
+        }
+        String username = userLocalDatabase.getString("username", "");
+        String password = userLocalDatabase.getString("password", "");
+
+        User user = new User(username, password);
+        return user;
+    }
+}
+
